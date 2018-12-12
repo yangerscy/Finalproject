aa027e8781c8b1f643c6b0d82ca051fa3c9d468c
yangerscy <yangerscy@gmail.com>
2018-12-12 20:41:57 +0800

    yangerscy

--- app/src/debug/java/com/example/me/finalproject/foodstreet.java
+++ app/src/debug/java/com/example/me/finalproject/foodstreet.java
@@ -1,19 +1,27 @@
 package com.example.me.finalproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class foodstreet extends AppCompatActivity {
+public class foodstreet extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodstreet);
-        List<cardlist> memberList = new ArrayList<>();
-        memberList.add(new cardlist(1, R.drawable.lauchbox, "白沙屯海灘1"));
 
-    }
-}
+
+
+}}
