13f2c917185eb3e684e73abf3909e4336eeff0b2
yangerscy <yangerscy@gmail.com>
2018-12-16 01:23:19 +0800

    yangerscy

--- app/src/debug/java/com/example/me/finalproject/storelist1.java
+++ app/src/debug/java/com/example/me/finalproject/storelist1.java
@@ -1,14 +1,23 @@
 package com.example.me.finalproject;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class storelist1 extends AppCompatActivity {
-
+public class storelist1 extends AppCompatActivity
+implements AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener
+{
+    String[] liststore ;
+    ListView  lisv;
+    ArrayAdapter<String> aa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,23 @@ public class storelist1 extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        lisv = findViewById(R.id.lists);
+        aa= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,liststore);
+        lisv.setAdapter(aa);
+        lisv.setOnItemClickListener(this);
+        lisv.setOnItemLongClickListener(this);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+       Intent listshow = new Intent(this,storelistshow.class);
+      startActivity(listshow);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        liststore[position] = (position+1) + ".";
+        aa.notifyDataSetChanged();
+        return true;
+    }
 }
