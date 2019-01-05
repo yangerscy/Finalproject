a7311b0287b22b1c24fad75b5d449a5395a20910
yangerscy <yangerscy@gmail.com>
2019-01-05 22:59:15 +0800

    yangerscy

--- app/src/debug/java/com/example/me/finalproject/storelist1.java
+++ app/src/debug/java/com/example/me/finalproject/storelist1.java
@@ -56,6 +56,7 @@ implements AdapterView.OnItemClickListener,View.OnClickListener,AdapterView.OnIt
         bt_queryall.setOnClickListener(this);
         bt_update.setOnClickListener(this);
         bt_query.setOnClickListener(this);
+        Toast.makeText(getApplicationContext(),"長按可刪除店家",Toast.LENGTH_LONG).show();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
